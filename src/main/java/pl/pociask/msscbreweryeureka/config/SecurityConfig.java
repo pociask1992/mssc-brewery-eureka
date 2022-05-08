b73941a54397a5dc447fb289ad72f20410f38219
dpociask <pociask1992@gmail.com>
2022-05-08 14:24:30 +0200

    adding security into eureka

---
+++ src/main/java/pl/pociask/msscbreweryeureka/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package pl.pociask.msscbreweryeureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
